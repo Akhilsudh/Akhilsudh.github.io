7336018df673c34a2a3a31fe69c8f1b2b801315b
Akhil <akhil.sudh@gmail.com>
2017-10-07 19:43:10 +0530

    Added cards for education and Experience

--- js/pagination.js
+++ js/pagination.js
@@ -1,37 +1,13 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n)
-{
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n)
-{
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n)
-{
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length)
-  {
-    slideIndex = 1
+function openTab(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
   }
-  if (n < 1)
-  {
-    slideIndex = x.length
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-amber", "");
   }
-  for (i = 0; i < x.length; i++)
-  {
-     x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++)
-  {
-     dots[i].className = dots[i].className.replace(" w3-red", "");
-  }
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " w3-red";
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " w3-amber";
 }
