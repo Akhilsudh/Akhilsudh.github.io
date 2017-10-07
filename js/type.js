c2d80bfbaa340726eac7a8f97babd4e73f325cf7
Akhil <akhil.sudh@gmail.com>
2017-10-07 14:19:12 +0530

    Major Overhaul

--- js/type.js
+++ js/type.js
@@ -53,4 +53,4 @@ var TxtType = function(el, toRotate, period) {
         css.type = "text/css";
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
-    };
+};
